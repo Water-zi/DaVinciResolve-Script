cae586ff95650b709f3ee75792ac769fe840345b
Water-zi <928069358@qq.com>
2023-12-29 10:56:23 +0800

    Fix copy error bug.

--- FinishRenderAutoCopy/main.py
+++ FinishRenderAutoCopy/main.py
@@ -33,8 +33,11 @@ def get_directory_size(directory):
     total_size = 0
     for dirpath, dirnames, filenames in os.walk(directory):
         for filename in filenames:
-            file_path = os.path.join(dirpath, filename)
-            total_size += os.path.getsize(file_path)
+            try:
+                file_path = os.path.join(dirpath, filename)
+                total_size += os.path.getsize(file_path)
+            except:
+                continue
     return total_size
 
 
